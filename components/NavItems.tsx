a04cf52ad059c1a41b12f6105a8ad3894ad1e1b7
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2025-10-11 16:27:55 +0500

    :sparkles: implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My Journey", href: "/my-journey" },
+];
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavItems = () => {
+  const pathName = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathName === href && "text-primary font-semibold")}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
