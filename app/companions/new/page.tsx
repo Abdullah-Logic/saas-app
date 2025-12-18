0c6ebc3f34f11158b691dffeee0df0001c8a90a6
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2025-12-19 01:22:36 +0500

    :wrench: fix code for deployment

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import CompanionForm from "@/components/CompanionForm";
 import { newCompanionPermissions } from "@/lib/actions/companion.actions";
 import { auth } from "@clerk/nextjs/server";
